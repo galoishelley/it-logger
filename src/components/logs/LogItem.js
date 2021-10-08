7189035347bdb275802c56e8b4033aad4b526736
galoishelley <gracemiao2017@gmail.com>
2021-10-09 10:44:58 +1100

    init

---
+++ src/components/logs/LogItem.js
@@ -0,0 +1,25 @@
+import React from 'react'
+// import Moment from 'react-moment';
+import PropTypes from 'prop-types'
+
+const LogItem = ({log}) => {
+    console.log(log);
+    return (
+        <li className="collection-item">
+            <div>
+                <a href="#edit-log-modal" className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'} `}>{log.message}</a>
+                {/* <br />
+                <span className="grey-text">
+                    <span className="black-text">ID #{log.id}</span> last updated by {' '}
+                    <spqn className="black-text">{log.tech}</spqn>
+                </span> */}
+            </div>
+        </li>
+    )
+}
+
+LogItem.propTypes = {
+    log: PropTypes.object.isRequired
+}
+
+export default LogItem;
